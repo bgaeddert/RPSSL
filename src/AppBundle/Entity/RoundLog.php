987cb8d5456a967e2f24c9d0a48eea3801910c39
Brian Gaeddert <bgaeddert@gmail.com>
2016-04-07 00:45:08 -0500

    Added RoundLog entity/repo

---
+++ src/AppBundle/Entity/RoundLog.php
@@ -0,0 +1,122 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * RoundLog
+ *
+ * @ORM\Table()
+ * @ORM\Entity(repositoryClass="AppBundle\Entity\RoundLogRepository")
+ */
+class RoundLog
+{
+    /**
+     * @ORM\ManyToOne(targetEntity="Gesture")
+     * @ORM\JoinColumn(name="human_gesture_id", referencedColumnName="id")
+     */
+    private $human_gesture;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="Gesture")
+     * @ORM\JoinColumn(name="comp_gesture_id", referencedColumnName="id")
+     */
+    private $comp_gesture;
+
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var boolean
+     *
+     * @ORM\Column(name="human_won", type="boolean")
+     */
+    private $humanWon;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set humanWon
+     *
+     * @param boolean $humanWon
+     * @return RoundLog
+     */
+    public function setHumanWon($humanWon)
+    {
+        $this->humanWon = $humanWon;
+
+        return $this;
+    }
+
+    /**
+     * Get humanWon
+     *
+     * @return boolean 
+     */
+    public function getHumanWon()
+    {
+        return $this->humanWon;
+    }
+
+    /**
+     * Set human_gesture
+     *
+     * @param \AppBundle\Entity\Gesture $humanGesture
+     * @return RoundLog
+     */
+    public function setHumanGesture(\AppBundle\Entity\Gesture $humanGesture = null)
+    {
+        $this->human_gesture = $humanGesture;
+
+        return $this;
+    }
+
+    /**
+     * Get human_gesture
+     *
+     * @return \AppBundle\Entity\Gesture 
+     */
+    public function getHumanGesture()
+    {
+        return $this->human_gesture;
+    }
+
+    /**
+     * Set comp_gesture
+     *
+     * @param \AppBundle\Entity\Gesture $compGesture
+     * @return RoundLog
+     */
+    public function setCompGesture(\AppBundle\Entity\Gesture $compGesture = null)
+    {
+        $this->comp_gesture = $compGesture;
+
+        return $this;
+    }
+
+    /**
+     * Get comp_gesture
+     *
+     * @return \AppBundle\Entity\Gesture 
+     */
+    public function getCompGesture()
+    {
+        return $this->comp_gesture;
+    }
+}
