09131b42e911221b94b36598f05da691d344c771
Brian Gaeddert <bgaeddert@gmail.com>
2016-04-06 22:45:07 -0500

    Capture resolve and return selected gesture

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -13,13 +13,21 @@ class DefaultController extends Controller
      */
     public function indexAction(Request $request)
     {
+        // INPUT
+        $human_gesture_id = intval( $request->request->get( 'gesture_id' ) );
+
         // Get all Gestures from DataBase
-        $gestures = $this->getDoctrine()
+        $gesture_options = $this->getDoctrine()
             ->getRepository( 'AppBundle:Gesture' )
             ->findAll();
 
+        $human_gesture = $this->getDoctrine()
+            ->getRepository( 'AppBundle:Gesture' )
+            ->find($human_gesture_id);
+
         return $this->render('default/index.html.twig',[
-            "gestures" => $gestures,
+            "gesture_options" => $gesture_options,
+            "human_gesture" => $human_gesture,
         ]);
     }
 }
