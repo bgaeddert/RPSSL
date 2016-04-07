d768cc2a9c57f3ad98a7a5e8f77cf7687f1c0376
Brian Gaeddert <bgaeddert@gmail.com>
2016-04-06 23:31:40 -0500

    Added references to rule data fixture

--- src/AppBundle/DataFixtures/ORM/LoadGestureData.php
+++ src/AppBundle/DataFixtures/ORM/LoadGestureData.php
@@ -1,10 +1,11 @@
 <?php
 
 use AppBundle\Entity\Gesture;
+use Doctrine\Common\DataFixtures\AbstractFixture;
 use Doctrine\Common\DataFixtures\FixtureInterface;
 use Doctrine\Common\Persistence\ObjectManager;
 
-class LoadGestureData implements FixtureInterface
+class LoadGestureData extends AbstractFixture implements FixtureInterface
 {
     public function load(ObjectManager $manager)
     {
@@ -12,22 +13,37 @@ class LoadGestureData implements FixtureInterface
         $rockGesture->setName('rock');
         $manager->persist($rockGesture);
 
+        $this->addReference('rock-gesture', $rockGesture);
+
         $paperGesture = new Gesture();
         $paperGesture->setName('paper');
         $manager->persist($paperGesture);
+
+        $this->addReference('paper-gesture', $paperGesture);
 
         $scissorsGesture = new Gesture();
         $scissorsGesture->setName('scissors');
         $manager->persist($scissorsGesture);
 
+        $this->addReference('scissors-gesture', $scissorsGesture);
+
         $lizardGesture = new Gesture();
         $lizardGesture->setName('lizard');
         $manager->persist($lizardGesture);
+
+        $this->addReference('lizard-gesture', $lizardGesture);
 
         $spockGesture = new Gesture();
         $spockGesture->setName('spock');
         $manager->persist($spockGesture);
 
+        $this->addReference('spock-gesture', $spockGesture);
+
         $manager->flush();
+    }
+
+    public function getOrder()
+    {
+        return 2;
     }
 }
