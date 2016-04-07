a56953379663886c2b4a95f8da5f0c3e21d4da06
Brian Gaeddert <bgaeddert@gmail.com>
2016-04-07 11:35:30 -0500

    Post route with error trapping

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -7,6 +7,7 @@ use AppBundle\Service\MetricsService;
 use AppBundle\Service\RoundLogService;
 use AppBundle\Entity\RoundLog;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 
@@ -14,37 +15,59 @@ class DefaultController extends Controller
 {
     /**
      * @Route("/", name="homepage")
+     * @Method("GET")
      */
-    public function indexAction(Request $request)
+    public function indexAction( Request $request )
     {
-        // INPUT
-        $human_gesture_id = intval($request->request->get('gesture_id'));
-
         // Get all Gestures from DataBase
-        $gesture_options = $this->get('gesture_service')->getOptions();
+        $gesture_options = $this->get( 'gesture_service' )->getOptions();
 
         // init load setting
         $response_array = [
-            'gesture_options' => $gesture_options,
-            'human_gesture' => null,
+            'gesture_options'  => $gesture_options,
+            'human_gesture'    => null,
             'computer_gesture' => null,
             'result_statement' => 'Ready player one.',
-            'metrics' => null,
+            'metrics'          => null,
         ];
 
-        if ($human_gesture_id) {
-            $game = $this->get('game_service')->play($gesture_options, $human_gesture_id);
+        // METRICS
+        $response_array[ 'metrics' ] = $this->get( 'metrics_service' )->get();
 
-            $response_array[ 'human_gesture' ] = $game->getHumanGesture();
-            $response_array[ 'computer_gesture' ] = $game->getComputerGesture();
-            $response_array[ 'result_statement' ] = $game->getResultStatement();
+        return $this->render( 'default/index.html.twig', $response_array );
+    }
 
-            $this->get('round_logger_service')->log($game);
+    /**
+     * @Route("/", name="play")
+     * @Method("POST")
+     */
+    public function playAction( Request $request )
+    {
+        // INPUT
+        $human_gesture_id = intval( $request->request->get( 'gesture_id' ) );
+
+        $human_gesture = $this->get( "gesture_repository" )->find( $human_gesture_id );
+
+        if ( $human_gesture == null )
+        {
+            $this->addFlash('error','Gesture not found!');
+            return $this->redirectToRoute( 'homepage' );
         }
 
-        // METRICS
-        $response_array[ 'metrics' ] = $this->get('metrics_service')->get();
+        // Get all Gestures from DataBase
+        $gesture_options = $this->get( 'gesture_service' )->getOptions();
+        $response_array[ 'gesture_options' ]  = $gesture_options;
 
-        return $this->render('default/index.html.twig', $response_array);
+        $game = $this->get( 'game_service' )->play( $gesture_options, $human_gesture_id );
+        $this->get( 'round_logger_service' )->log( $game );
+
+        $response_array[ 'human_gesture' ]    = $game->getHumanGesture();
+        $response_array[ 'computer_gesture' ] = $game->getComputerGesture();
+        $response_array[ 'result_statement' ] = $game->getResultStatement();
+
+        // METRICS
+        $response_array[ 'metrics' ] = $this->get( 'metrics_service' )->get();
+
+        return $this->render( 'default/index.html.twig', $response_array );
     }
 }
