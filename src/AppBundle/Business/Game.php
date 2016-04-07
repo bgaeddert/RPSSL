06755e4b49ffbd5e7716d5b4581cac483c5218a7
Brian Gaeddert <bgaeddert@gmail.com>
2016-04-07 00:30:58 -0500

    Added Game logic

---
+++ src/AppBundle/Business/Game.php
@@ -0,0 +1,107 @@
+<?php namespace AppBundle\Business;
+
+
+use AppBundle\Entity\Gesture;
+use AppBundle\Entity\GestureRepository;
+use AppBundle\Entity\RuleRepository;
+
+class Game
+{
+    /**
+     * @var GestureRepository
+     */
+    private $gesture_repository;
+
+    /**
+     * @var RuleRepository
+     */
+    private $rule_repository;
+
+    /**
+     * @var Gesture
+     */
+    private $human_gesture;
+
+    /**
+     * @var Gesture
+     */
+    private $computer_gesture;
+
+    /**
+     * @var string
+     */
+    private $result_statement;
+
+
+    /**
+     * Game constructor.
+     * @param GestureRepository $gesture_repository
+     * @param RuleRepository $rule_repository
+     */
+    public function __construct( GestureRepository $gesture_repository, RuleRepository $rule_repository )
+    {
+        $this->gesture_repository = $gesture_repository;
+        $this->rule_repository    = $rule_repository;
+    }
+
+    /**
+     * @param $gesture_options
+     * @param $human_gesture_id
+     * @return $this
+     */
+    public function play( $gesture_options, $human_gesture_id )
+    {
+        $this->result_statement = "Game end in a tie";
+
+        $this->human_gesture    = $this->gesture_repository->find( $human_gesture_id );
+        $this->computer_gesture = $gesture_options[ array_rand( $gesture_options, 1 ) ];
+
+        $human_wins = $this->rule_repository
+            ->findByGestures( $this->human_gesture, $this->computer_gesture );
+
+        // Default result
+
+        // Return if human wins
+        if ( $human_wins )
+        {
+            $this->result_statement = "You won - {$human_wins->getDescription()}";
+
+            return $this;
+        }
+
+        $computer_wins = $this->rule_repository
+            ->findByGestures( $this->computer_gesture, $this->human_gesture );
+
+        if ( $computer_wins )
+        {
+            $this->result_statement = "Computer won - {$computer_wins->getDescription()}";
+        }
+
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getHumanGesture()
+    {
+        return $this->human_gesture;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getComputerGesture()
+    {
+        return $this->computer_gesture;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getResultStatement()
+    {
+        return $this->result_statement;
+    }
+
+}
