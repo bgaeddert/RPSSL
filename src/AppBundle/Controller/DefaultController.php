3fad1082796e170a3221acfcf5295248bec143d2
Brian Gaeddert <bgaeddert@gmail.com>
2016-04-09 23:20:13 -0500

    Refactor / comment

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -19,19 +19,19 @@ class DefaultController extends Controller
      * @param Request $request
      * @return \Symfony\Component\HttpFoundation\Response
      */
-    public function indexAction( Request $request )
+    public function indexAction(Request $request)
     {
         // Get all Gestures from DataBase
-        $gesture_options = $this->get( 'gesture_service' )->getOptions();
+        $gesture_options = $this->get('gesture_service')->getOptions();
 
         // Build response array
         $response_array[ 'gesture_options' ]  = $gesture_options;
         $response_array[ 'human_gesture' ]    = null;
         $response_array[ 'computer_gesture' ] = null;
         $response_array[ 'result_statement' ] = 'Ready player one.';
-        $response_array[ 'metrics' ]          = $this->get( 'metrics_service' )->get();
+        $response_array[ 'metrics' ]          = $this->get('metrics_service')->get();
 
-        return $this->render( 'default/index.html.twig', $response_array );
+        return $this->render('default/index.html.twig', $response_array);
     }
 
     /**
@@ -40,35 +40,39 @@ class DefaultController extends Controller
      * @param Request $request
      * @return \Symfony\Component\HttpFoundation\RedirectResponse|\Symfony\Component\HttpFoundation\Response
      */
-    public function playAction( Request $request )
+    public function playAction(Request $request)
     {
         // Resolve input
-        $human_gesture_id = intval( $request->request->get( 'gesture_id' ) );
-        $human_gesture    = $this->get( "gesture_repository" )->find( $human_gesture_id );
+        $human_gesture_id = intval($request->request->get('gesture_id'));
+        $human_gesture    = $this->get("gesture_repository")->find($human_gesture_id);
 
-        // Validate
-        if ( $human_gesture == null )
-        {
-            $this->addFlash( 'error', 'Gesture not found!' );
-            return $this->redirectToRoute( 'homepage' );
+        // Validate - If provided human gesture id not found
+        // redirect with error
+        if ($human_gesture == null) {
+            $this->addFlash('error', 'Gesture not found!');
+            return $this->redirectToRoute('homepage');
         }
 
         // Get all Gestures from DataBase
-        $gesture_options = $this->get( 'gesture_service' )->getOptions();
+        $gesture_options = $this->get('gesture_service')->getOptions();
+
+        // Choose random gesture for computer
+        // from all gesture options
+        $computer_gesture = $gesture_options[ array_rand($gesture_options, 1) ];
 
         // Run game logic
-        $round = $this->get( 'round_service' )->play( $gesture_options, $human_gesture );
+        $round = $this->get('round_service')->play($human_gesture, $computer_gesture);
 
         // Log results
-        $this->get( 'round_logger_service' )->log( $round );
+        $this->get('round_logger_service')->log($round);
 
         // Build response array
         $response_array[ 'gesture_options' ]  = $gesture_options;
         $response_array[ 'human_gesture' ]    = $round->getHumanGesture();
         $response_array[ 'computer_gesture' ] = $round->getComputerGesture();
         $response_array[ 'result_statement' ] = $round->getResultStatement();
-        $response_array[ 'metrics' ]          = $this->get( 'metrics_service' )->get();
+        $response_array[ 'metrics' ]          = $this->get('metrics_service')->get();
 
-        return $this->render( 'default/index.html.twig', $response_array );
+        return $this->render('default/index.html.twig', $response_array);
     }
 }
