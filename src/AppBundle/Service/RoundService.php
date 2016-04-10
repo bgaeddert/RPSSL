fd145642aeafdc66f2c0ca9544a8b0baddad32aa
Brian Gaeddert <bgaeddert@gmail.com>
2016-04-09 23:19:15 -0500

    Refactor / comment - limit responsibility

--- src/AppBundle/Service/RoundService.php
+++ src/AppBundle/Service/RoundService.php
@@ -42,47 +42,84 @@ class RoundService
      * Game constructor.
      *
      * @param GestureRepository $gesture_repository
-     * @param RuleRepository    $rule_repository
+     * @param RuleRepository $rule_repository
      */
     public function __construct(GestureRepository $gesture_repository, RuleRepository $rule_repository)
     {
         $this->gesture_repository = $gesture_repository;
-        $this->rule_repository = $rule_repository;
+        $this->rule_repository    = $rule_repository;
     }
 
     /**
-     * @param $gesture_options
+     * Determines winner Human or Computer
+     *
      * @param Gesture $human_gesture
+     * @param Gesture $computer_gesture
      * @return $this
      */
-    public function play($gesture_options, Gesture $human_gesture)
+    public function play(Gesture $human_gesture, Gesture $computer_gesture)
     {
-        // Default result
+        // Set gestures
+        $this->human_gesture    = $human_gesture;
+        $this->computer_gesture = $computer_gesture;
 
-        $this->result_statement = 'Game ended in a tie';
-
-        $this->human_gesture = $this->gesture_repository->find($human_gesture->getId());
-        $this->computer_gesture = $gesture_options[ array_rand($gesture_options, 1) ];
-
-        $human_wins = $this->rule_repository
-            ->findByGestures($this->human_gesture, $this->computer_gesture);
-
-        // Return if human wins
-        if ($human_wins) {
-            $this->result_statement = "You won - {$human_wins->getDescription()}";
-            $this->human_won = true;
-
+        if ($this->humanWins()) {
             return $this;
         }
 
-        $computer_wins = $this->rule_repository
-            ->findByGestures($this->computer_gesture, $this->human_gesture);
-
-        if ($computer_wins) {
-            $this->result_statement = "Computer won - {$computer_wins->getDescription()}";
+        if ($this->computerWins()) {
+            return $this;
         }
 
+        // Default result ( no winner found )
+        $this->result_statement = 'Game ended in a tie';
+        $this->human_won        = false;
+
         return $this;
+    }
+
+    /**
+     * Since only winning gestures exist in the database
+     * the human wins if a row is found with this provided
+     * combination of [ human gesture, computer gesture ]
+     *
+     * @return bool
+     */
+    private function humanWins()
+    {
+        $winning_rule_found = $this->rule_repository
+            ->findByGestures($this->human_gesture, $this->computer_gesture);
+
+        if ($winning_rule_found) {
+            $this->result_statement = "You won - {$winning_rule_found->getDescription()}";
+            $this->human_won        = true;
+
+            return true;
+        }
+
+        return false;
+    }
+
+    /**
+     * Since only winning gestures exist in the database
+     * the computer wins if a row is found with the inversion
+     * of the provided combination to [ computer gesture, human gesture ]
+     *
+     * @return bool
+     */
+    private function computerWins()
+    {
+        $winning_rule_found = $this->rule_repository
+            ->findByGestures($this->computer_gesture, $this->human_gesture);
+
+        if ($winning_rule_found) {
+            $this->result_statement = "Computer won - {$winning_rule_found->getDescription()}";
+            $this->human_won        = false;
+
+            return true;
+        }
+
+        return false;
     }
 
     /**
